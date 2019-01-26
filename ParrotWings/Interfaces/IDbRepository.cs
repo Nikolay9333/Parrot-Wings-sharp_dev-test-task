540df9dfd4cfaa3338cfbf4e476a12bab7c7caf1
niko <zadorozhnyyn@list.ru>
2019-01-26 22:44:01 +0300

    Добавил интерфейс для круд репозитория

---
+++ ParrotWings/Interfaces/IDbRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace ParrotWings.Interfaces
+{
+    public interface IDbRepository
+    {
+        T Get<T>(bool noTracking = true);
+        IEnumerable<T> GetAll<T>(bool noTracking = true);
+        T Add<T>(T entity);
+        T Delete<T>(T entity);
+    }
+}
